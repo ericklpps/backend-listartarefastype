0697bc95171e95d7970d10db39a39029ef35d1e8
ericklpps <ericklpsilva@gmail.com>
2024-09-18 20:07:24 -0300

    atualizando backend com banco de dados

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 // Cria um banco de dados SQLite em memória
-const db = new sqlite3.Database(':memory:'); // Use um caminho de arquivo para persistência
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('lista-tarefas.db');
 
 // Cria a tabela 'tarefas' no banco de dados
 db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)");
 });
 
-app.use(express.json());
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
+});
+
 app.post('/tarefas', (req, res) => {
     const { tarefa } = req.body;
-    db.run("INSERT INTO tarefas (tarefa) VALUES (?)", [tarefa], function (err) {
+    db.run("INSERT INTO tarefas (tarefa) VALUES (?)", [tarefa], function(err) {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
         res.status(201).json({ id: this.lastID, tarefa });
     });
 });
+
 app.get('/tarefas', (req, res) => {
     db.all("SELECT * FROM tarefas", [], (err, rows) => {
         if (err) {
@@ -30,6 +35,7 @@ app.get('/tarefas', (req, res) => {
         res.status(200).json(rows);
     });
 });
+
 app.get('/tarefas/:id', (req, res) => {
     const { id } = req.params;
     db.get("SELECT * FROM tarefas WHERE id = ?", [id], (err, row) => {
@@ -43,10 +49,12 @@ app.get('/tarefas/:id', (req, res) => {
         }
     });
 });
+
+
 app.put('/tarefas/:id', (req, res) => {
     const { id } = req.params;
     const { tarefa } = req.body;
-    db.run("UPDATE tarefas SET tarefa = ? WHERE id = ?", [tarefa, id], function (err) {
+    db.run("UPDATE tarefas SET tarefa = ? WHERE id = ?", [tarefa, id], function(err) {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
@@ -57,9 +65,10 @@ app.put('/tarefas/:id', (req, res) => {
         }
     });
 });
+
 app.delete('/tarefas/:id', (req, res) => {
     const { id } = req.params;
-    db.run("DELETE FROM tarefas WHERE id = ?", [id], function (err) {
+    db.run("DELETE FROM tarefas WHERE id = ?", [id], function(err) {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
@@ -70,3 +79,4 @@ app.delete('/tarefas/:id', (req, res) => {
         }
     });
 });
+
