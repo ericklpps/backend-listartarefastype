c5eb6b7170dc3e6a75ff5cecb7044c58c72cf527
ericklpps <ericklpsilva@gmail.com>
2024-09-20 22:36:18 -0300

    Correção do userController e instalção de dependências

--- db/database.js
+++ db/database.js
@@ -1,3 +1,11 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('lista-tarefas.db');
+
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)");
+});
+
 db.run(`
   CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER PRIMARY KEY,
@@ -6,3 +14,6 @@ db.run(`
     role TEXT
   )
 `);
+
+
+module.exports = db;
