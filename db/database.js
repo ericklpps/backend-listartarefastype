ecb6fc9e50c32a6162902342dcbb72599c9809f2
ericklpps <ericklpsilva@gmail.com>
2024-09-20 21:58:10 -0300

    Aplicando autenticação eautorização no backend

--- db/database.js
+++ db/database.js
@@ -1,9 +1,8 @@
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('lista-tarefas.db');
-
-db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)");
-});
-
-module.exports = db;
+db.run(`
+  CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    role TEXT
+  )
+`);
