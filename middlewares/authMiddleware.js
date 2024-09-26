2fe83b5a8cd8c1a2975b92f2585c72ef41006db7
ericklpps <ericklpsilva@gmail.com>
2024-09-25 22:39:17 -0300

    Alteração nos autenticadores e nas tarefas, ajustando os tokens corretamente

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,20 +1,23 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers['authorization'];
+  const token = req.headers['authorization'];
 
-    if (!token) {
-        return res.status(403).json({ error: 'Nenhum token fornecido!' });
+  if (!token) {
+    return res.status(403).json({ error: 'Nenhum token fornecido!' });
+  }
+
+  // Separar 'Bearer' do token
+  const bearerToken = token.split(' ')[1];
+
+  jwt.verify(bearerToken, 'secreta-chave', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Token inválido!' });
     }
 
-    jwt.verify(token, 'secreta-chave', (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ error: 'Token inválido!' });
-        }
-
-        req.user = decoded;
-        next();
-    });
+    req.user = decoded;
+    next();
+  });
 };
 
 module.exports = authMiddleware;
